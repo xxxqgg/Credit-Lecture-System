dd0bdaa0ef4249fcbfb398c069dcca735ae0f8bd
Jerrypoi <jerry.poi.sz@gmail.com>
2020-02-05 01:31:59 +0800

    user model: sign out view

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
 from .views import (
-    index_view
+    index_view,
+    signout_view,
 )
 app_name = 'user'
 urlpatterns = [
     path('', index_view, name='index'),
+    path('signout/',signout_view, name='signout'),
 ]
--- src/user/views.py
+++ src/user/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
+from django.contrib.auth import logout
 
 
 # Create your views here.
 def index_view(request):
     return HttpResponse(str(request.user))
+
+
+def signout_view(request):
+    logout(request)
+    return redirect('admin:login')
